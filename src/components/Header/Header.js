648897895e9529a452e2a47c5c46e1c58adc55cb
Karo Zakaryan <karo.zakaryan.y@tumo.org>
2018-12-27 01:52:30 +0400

    Setup materialUI,add Header,SearchBox,FilterBox... initial components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import SearchBox from "./SearchBox/SearchBox";
+import { withStyles } from '@material-ui/core/styles';
+import FilterBox from "./FilterBox/FilterBox";
+
+const Header = props => {
+    const { classes } = props;
+
+    return (
+        <header>
+            <nav className={classes.root}>
+                <AppBar position="fixed">
+                    <Toolbar>
+                        <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                            PokeDex
+                        </Typography>
+                        <div className={classes.grow} />
+                        <FilterBox/>
+                        <div className={classes.grow} />
+                        <SearchBox/>
+                    </Toolbar>
+                </AppBar>
+            </nav>
+        </header>
+    );
+};
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    title: {
+        fontSize: 25,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+});
+
+export default withStyles(styles)(Header);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import * as serviceWorker from './serviceWorker';
 import PokeDexApp from './PokeDexApp';
 import {Provider} from "react-redux"
 import {store} from "./redux/store";
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import './index.css';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {main: "#2196f3"},
+        secondary: {main: '#3f51b5'},
+    },
+    typography: {useNextVariants: true},
+});
 
 ReactDOM.render(
     <Provider store={store}>
-        <PokeDexApp/>
+        <MuiThemeProvider theme={theme}>
+            <PokeDexApp/>
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById('root')
 );
